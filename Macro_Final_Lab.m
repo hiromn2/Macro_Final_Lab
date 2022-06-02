dfa7c474924ed93e9e9a9104736f57a9908bc5ac
IvanZamorano <ivan_zamorano_rojas@hotmail.com>
2022-06-02 07:59:30 -0500

    Update Macro_Final_Lab.m

--- Macro_Final_Lab.m
+++ Macro_Final_Lab.m
@@ -1,7 +1,7 @@
-% Aprox discreta
+% Aproximación discreta/Discrete Aproximation
 clc; clear all; close all;
 
-% Parámetros del modelo:
+% Parámetros del modelo/Model Paremeters:
 
 alpha = 0.36; % participación del capital en la producción
 beta = 0.96; % factor de descuento
@@ -11,6 +11,50 @@ rho = 0.95
 sigma_e = 0.004
 
 %% Inciso i): Método de Tauchen
+
+%Método de Tauchen 
+
+function [grid,P] = Tauchen(rho,N,sigma)
+
+% Discretiza un proceso estocástico AR(1), dados sus parámetros
+
+% Notación:
+
+% rho = persistencia del proceso AR(1).
+% N = número de puntos en la malla discreta.
+% sigma = desviación estándar de las innovaciones.
+
+% grid = malla del AR(1) discreto.
+% P = matriz de transición.
+
+        % Construyendo la malla de valores
+        sigma_z = sqrt(sigma^2/(1-rho^2)); 
+        step =(2*sigma_z*3)/(N-1); % distancia entre puntos
+        grid = zeros(N,1); % inicializa la malla
+        for i=1:N
+            grid(i) = -3*sigma_z+ (i-1)*step; % obtiene cada punto de la malla
+        end
+        
+        % Construyendo la matriz de transición
+        
+        P = zeros(N,N); % inicializa la matriz P
+        if N>1 
+           for i=1:N
+            P(i,1)=normcdf((grid(1)+step/2-rho*grid(i))/sigma); % usando la distribución normal
+            P(i,N)=1-normcdf((grid(N)-step/2-rho*grid(i))/sigma);
+                for j=2:N-1
+                P(i,j)=normcdf((grid(j)+step/2-rho*grid(i))/sigma) ...
+                    -normcdf((grid(j)-step/2-rho*grid(i))/sigma);
+                end
+           end
+        else
+            P=1;
+        end
+        
+end
+
+
+
 
 
 % Definimos el número de puntos de la malla del choque
@@ -121,9 +165,9 @@ ylabel('C')
 
 
 
-% Regra de Decisao optima
-% Trajetoria de consumo
-% Funcao valor
+% Regra de Decisión optima
+% Trajectoria de consumo
+% Función valor
 
 %% Incisos iii): Simulación de la economía
 
@@ -229,4 +273,6 @@ plot(amalla,F(:,5))
 title('\zeta_5 = 0.6')
 xlabel('a');
 
-%% Inciso 4) criatividad!
+%% Inciso 4) creatividad!
+
+
